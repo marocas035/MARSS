b6a2dc9be56a3aadf804a8262d0fe179efa0dc49
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-06-23 09:15:54 +0200

    Update ca.py

--- ca.py
+++ ca.py
@@ -571,7 +571,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='ca parser')
     parser.add_argument('-an', '--agent_number', type=int, metavar='', required=False, default=1, help='agent_number: 1,2,3,4..')
     parser.add_argument('-w', '--wait_msg_time', type=int, metavar='', required=False, default=20, help='wait_msg_time: time in seconds to wait for a msg. Purpose of system monitoring')
-    parser.add_argument('-st', '--stop_time', type=int, metavar='', required=False, default=84600, help='stop_time: time in seconds where agent')
+    parser.add_argument('-st', '--stop_time', type=int, metavar='', required=False, default=84600, help='stop_time: time in seconds where agent isnt asleep')
     parser.add_argument('-s', '--status', type=str, metavar='', required=False, default='stand-by', help='status_var: on, stand-by, Off')
     parser.add_argument('-sab', '--start_auction_before', type=int, metavar='', required=False, default=10, help='start_auction_before: seconds to start auction prior to current fab ends')
     parser.add_argument('--search', type=str, metavar='', required=False, default='No',help='Search order by code.Write depending on your case: oc (order_code), sg(steel_grade),at(average_thickness), wi(width_c>    args = parser.parse_args()
