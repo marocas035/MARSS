04b1e861beebb51a2cb0c9dd650793fee46482ea
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-06-29 19:59:44 +0200

    Update browser.py

--- browser.py
+++ browser.py
@@ -64,7 +64,7 @@ class BrowserAgent(Agent):
                             code_to_search = int(c[1])
                         elif type_code_to_search == 'sg':
                             column = 'Steel_grade'
-                            code_to_search = int(c[1])
+                            code_to_search = c[1]
                         elif type_code_to_search == 'at':
                             column = 'Thickness'
                             code_to_search = float(c[1])
