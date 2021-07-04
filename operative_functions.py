ee0561638d0246383db2c87575e9a16f3392d0b1
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-07-04 09:46:43 +0200

    Update operative_functions.py

--- operative_functions.py
+++ operative_functions.py
@@ -137,7 +137,7 @@ def active_agents(id, *code):
     dff = dff.drop_duplicates(keep='first')
     dff.to_csv('ActiveAgents.csv', header = True, index = False)
     
- def msg_to_launcher(msg_body, agent_directory):
+ def msg_to_launcher(msg_body):
     """Returns msg object to send to launcher agent"""
     agents_df = agents_data()
     agents_df = agents_df.loc[agents_df['Name'] == "launcher"]
