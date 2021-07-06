a0317347ac4dbc5b7f63573d60199c713b93b7be
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-07-06 17:32:00 +0200

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -68,7 +68,8 @@ if __name__ == "__main__":
     parser.add_argument('-w', '--wait_msg_time', type=int, metavar='', required=False, default=10, help='wait_msg_time: time in seconds to wait for a msg')
     parser.add_argument('-st', '--stop_time', type=int, metavar='', required=False, default=10, help='stop_time: time in seconds where agent')
     parser.add_argument('-s', '--status', type=str, metavar='', required=False, default='stand-by', help='status_var: on, stand-by, Off')
-    parser.add_argument('--search', type=str, metavar='', required=False, default='No',help='Search order by code. Writte depending on your case: oc (order_code),sg(steel_grade),at(average_thickness), wi(width_>    #DATOS DE PEDIDO:
+    parser.add_argument('--search', type=str, metavar='', required=False, default='No',help='Search order by code. Writte depending on your case: oc (order_code),sg(steel_grade),at(average_thickness), wi(width_coils), ic(id_coil), so(string_operations), date.Example: --search oc = 987')
+    #DATOS DE PEDIDO:
     parser.add_argument('-oc', '--order_code', type=str, metavar='',required=False, default='No', help='Specify the number code of the order. Write between "x"')
     parser.add_argument('-sg', '--steel_grade', type=str, metavar='', required=False, default='1', help='Number which specifies the type of steel used for coils in an order.Write between "x"')
     parser.add_argument('-at', '--average_thickness', type=float, metavar='', required=False, default='0.4',help='Specify the thickness for coils ordered')
@@ -76,7 +77,7 @@ if __name__ == "__main__":
     parser.add_argument('-nc', '--number_coils', type=int, metavar='', required=False, default='1', help='Number of coils involved in the order')
     parser.add_argument('-lc', '--list_coils', type=str, metavar='', required=False, default='No', help='List of codes of coils involved in the order.Write between "x"')
     parser.add_argument('-po', '--price_order', type=float, metavar='', required=False, default='1', help='Price given to the order')
-    parser.add_argument('-so', '--string_operations', type=str, metavar='', required=False, default='No', help='Sequence of operations needed.Write between "x".Format:"BZA|TD[2]|ENT[2|3]|HO[1|2]|NWW[1|4]|VA*[9|>
+    parser.add_argument('-so', '--string_operations', type=str, metavar='', required=False, default='No', help='Sequence of operations needed.Write between "x".Format:"BZA|TD[2]|ENT[2|3]|HO[1|2]|NWW[1|4]|VA*[9|10|11]"')    
     args = parser.parse_args()
     my_dir = os.getcwd()
     my_name = os.path.basename(__file__)[:-3]
