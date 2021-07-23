0479f242ca029dae88bd83830ebde1a02bb3896e
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-07-23 21:35:50 +0200

    Update log.py

--- log.py
+++ log.py
@@ -87,7 +87,7 @@ class LogAgent(Agent):
                         if aa[0] == 'SearchAA':  #Active agents list requested
                             logger.info(msg.body)
                             list_AA = active_agents.to_json
-                            log_msg_br = opf.msg_to_launcher(list_AA, my_dir)
+                            log_msg_br = opf.msg_to_browser(list_AA, my_dir)
                             await self.send(log_msg_br)                        
                 else:
                     logger.debug(f"Log_agent didn't receive any msg in the last {wait_msg_time}s") ####corregir, wait_msg_time es muy poco tiempo
