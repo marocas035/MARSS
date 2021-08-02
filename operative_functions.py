211217cbbd285827c9ba2452081a7a522f582fe3
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-08-02 20:32:30 +0200

    Update operative_functions.py

--- operative_functions.py
+++ operative_functions.py
@@ -85,12 +85,12 @@ def order_to_search(search_body,agent_full_name , agent_directory):
     search_msg.set_metadata("performative", "inform")
     return search_msg
 
-def order_searched(filter,agent_request,agent_directory, aa = 'Null'):
+def order_searched(filter,agent_request,agent_directory, aa = None):
     agents_df = agents_data()
     agents_df = agents_df.loc[agents_df['Name'] == agent_request]
     agent_jid = agents_df['User name'].iloc[-1]
     order_searched_msg = Message(to=agent_jid)
-    #if aa != 'Null':
+    #if aa is not None:
      #   order_searched_msg.body = 'Active Agent List:'+ str(filter)
     order_searched_msg.body = 'Search requested:'+ filter
     order_searched_msg.set_metadata("performative","inform")
