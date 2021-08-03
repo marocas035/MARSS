cc094e5b38e6a3aea10de915f66bf21f1c9983ca
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-08-03 18:29:24 +0200

    Update ca.py

--- ca.py
+++ ca.py
@@ -34,7 +34,7 @@ class ContinuousAnnealingAgent(Agent):
                     msg_aa_response = f'ActiveAgent: agent_name:{my_full_name}, active_time:{ca_status_started_at}'
                     response_active = opf.msg_to_log(msg_aa_response, my_dir)
                     await self.send(response_active)
-                elif single[0] == "Order searched":
+                elif single[0] == "Search requested":
                     print(msg.body)
             if ca_status_var == "pre-auction":
                 pre_auction_start = datetime.datetime.now()
