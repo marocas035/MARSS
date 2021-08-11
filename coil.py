3fb6969ba81721c48ae4df2cc97fa32658b4d680
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-08-11 16:59:35 +0200

    Update coil.py

--- coil.py
+++ coil.py
@@ -364,8 +364,6 @@ if __name__ == "__main__":
     parser.add_argument('--search', type=str, metavar='', required=False, default='No',help='Search order by code. Write depending on your case: aa=list (list active agents), oc(order_code), sg(steel_grade),at(average_thickness), wi(width_coils), ic(id_coil), so(string_operations), date. Example: --search oc=cO202106101')
     parser.add_argument('-set', '--search_time', type=int, metavar='', required=False, default=20, help='search_time: time in seconds where agent is searching by code')
     parser.add_argument('-do', '--delete', type=str, metavar='', required=False, default='No', help='Delete order in register given a code to filter')
-    parser.add_argument('-s', '--status', type=str, metavar='', required=False, default='stand-by', help='status_var: on, stand-by, off')
-    parser.add_argument('-b', '--budget', type=int, metavar='', required=False, default=200, help='budget: in case of needed, budget can be increased')
     parser.add_argument('-l', '--location', type=str, metavar='', required=False, default='K', help='location: K')
     parser.add_argument('-c', '--code', type=str, metavar='', required=False, default='cO202106101',help='code: cO202106101')
     parser.add_argument('-w', '--wait_auction_time', type=int, metavar='', required=False, default=500, help='wait_msg_time: time in seconds to wait for a msg')
