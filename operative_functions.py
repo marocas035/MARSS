b1e726e07ebeb82ff2f4ee8e8709695d690c3adf
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-08-21 17:16:27 +0200

    Update operative_functions.py

--- operative_functions.py
+++ operative_functions.py
@@ -271,6 +271,15 @@ def contact_list_json(contact_list,agent_directory):
     contact_list_msg.set_metadata("performative", "inform")
     return contact_list_msg
 
+def rq_contact_list_br_json(rq_contact_list,agent_directory):
+    agents_df = agents_data()
+    agents_df = agents_df.loc[agents_df['Name'] == "browser"]
+    browserer_jid = agents_df['User name'].iloc[-1]
+    contact_list_msg = Message(to=browser_jid)
+    contact_list_msg.body = rq_contact_list
+    contact_list_msg.set_metadata("performative", "inform")
+    return contact_list_msg
+
 def order_coil(la_json, code):
     df = pd.read_csv(f'agents.csv', header=0, delimiter=",", engine='python')
     name = df.loc[df.Code == code, 'User name'].values
