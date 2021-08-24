9a8c7caf0cbb30f9f3303c316e4d79d7ed053f6f
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-08-24 18:59:42 +0200

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -34,7 +34,7 @@ class LaunchAgent(Agent):
                 msg_cl = await self.receive(timeout=wait_msg_time) # wait for a message for 5 seconds
                 if msg_cl:
                     contact_list = msg_cl.body
-                    opf.change_warehouse(la_inform_log, my_dir) #,contact_list)
+                    opf.change_warehouse(la_inform_log_json, my_dir) #,contact_list)
                     
             if name_coil != "No":
                 la_coil_json = opf.order_budget(change_budget, name_coil).to_json(orient="records")
