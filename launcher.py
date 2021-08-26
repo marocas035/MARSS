9b2fed5b08fa228c7b91a41c1892c3f5bdbc0c5d
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-08-26 11:58:03 +0200

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -34,7 +34,7 @@ class LaunchAgent(Agent):
                 msg_cl = await self.receive(timeout=wait_msg_time) # wait for a message for 5 seconds
                 if msg_cl:
                     agent_df = pd.read_json(msg.body)
-                    if agent_df.loc[0, 'purpose'] == "contact_list"
+                    if agent_df.loc[0, 'purpose'] == "contact_list":
                         contact_list = msg_cl.body
                         print(contact_list)
                         opf.change_warehouse(la_inform_log, my_dir) #,contact_list)
