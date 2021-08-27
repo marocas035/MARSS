7847fce0d35b3f5b26c8397bfbb221878bee4aca
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-08-27 17:24:15 +0200

    Update browser.py

--- browser.py
+++ browser.py
@@ -74,7 +74,7 @@ class BrowserAgent(Agent):
                         if msg_cl:
                             agent_df = pd.read_json(msg_cl.body)
                             print(len(agent_df.columns))
-                            if (len(agent_df.columns)) == 2:
+                            if (len(agent_df.columns)) == 5:
                                 contact_list = agent_df.loc[0, 'msg1']
                                 active_coil_df = agent_df.loc[0, 'msg2']
                                 cl_to_launcher = opf.rec_list_la(my_full_name, contact_list, active_coil_df).to_json(orient="records")
