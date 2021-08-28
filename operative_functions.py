90f2febf24ac1a7ccf54c31305b01e4745045fb2
marocas035 <84638150+marocas035@users.noreply.github.com>
2021-08-28 16:31:53 +0200

    Update operative_functions.py

--- operative_functions.py
+++ operative_functions.py
@@ -352,7 +352,7 @@ def change_warehouse(launcher_df, my_dir, list_id_coil_agents, *args):
     wait_time = int(launcher_df.loc[0, 'wait_time'])
     if args:
         active_coil_str = str(args)
-        active_coil_split = active_coil_str.split("'")
+        active_coil_split = active_coil_str[1].split("'")
         active_coil_df = pd.DataFrame([], columns = ['coil_id', 'coil_agent_name', 'coil_jid' ,'coil_location'])
         active_coil_df = active_coil_df.append(active_coil_split, ignore_index=True)
         active_coil_df = active_coil_df.drop_duplicates(['coil_id', 'coil_agent_name'], keep='first')
